378f214c9567a2ff34f6eac2ad1843d24e66527f
Matt Abreu <mrmattabreu@gmail.com>
2020-08-01 18:40:53 +0100

    task(Signup): add sign in button

--- src/form/Signup.js
+++ src/form/Signup.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import "./Signup.css";
 
-function SignupForm() {
+function SignupForm(props) {
   return (
     <div className="box-signup">
       <h1>Sign up</h1>
 
       <form className="form-content"></form>
+
+      <span className="btn-signin" onClick={props.changeAuthType}>
+        Sign In
+      </span>
     </div>
   );
 }

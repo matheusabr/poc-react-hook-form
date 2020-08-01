232545b48ebaf1e24ba7d760cae485071368df7a
Matt Abreu <mrmattabreu@gmail.com>
2020-08-01 17:17:03 +0100

    task(Login): handle input data and submit action

--- src/form/Login.js
+++ src/form/Login.js
@@ -1,21 +1,29 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import "./Login.css";
 
 function LoginForm() {
+  const loginForm = useForm();
+  const { register, handleSubmit } = loginForm;
+
+  function onSubmit(data, event) {
+    console.log("data, event", data, event);
+  }
+
   return (
     <div className="box">
       <h1>Sign in</h1>
 
-      <form className="form-content">
+      <form className="form-content" onSubmit={handleSubmit(onSubmit)}>
         <div className="field">
           <label htmlFor="username">Username</label>
-          <input name="username" type="text" />
+          <input name="username" type="text" ref={register} />
         </div>
 
         <div className="field">
           <label htmlFor="password">Password</label>
-          <input name="password" type="text" />
+          <input name="password" type="text" ref={register} />
         </div>
 
         <button type="submit" className="btn-submit">
